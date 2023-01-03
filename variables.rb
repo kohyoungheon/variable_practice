4036db4d03efe92fc360ef2849f974a70eef936f
Young Koh <youngheon6@gmail.com>
2023-01-03 13:09:59 -0700

    add additional examples

--- variables.rb
+++ variables.rb
@@ -1,2 +1,20 @@
-word = "string number one"
-wordtwo = "string number two"
+string_one = "string number one"
+string_two = "string number two"
+
+integer_one = 2500
+integer_two = 5500
+
+float_one = 0.0001
+float_two = 0.000001
+
+boolean_one = True
+boolean_two = False
+boolean_three = "What?"
+
+#Reassigning the first two string variables
+string_one = "Reassigned string one"
+string_two = "Reassigned string two"
+
+#Print strings to terminal
+puts string_one
+puts string_two
